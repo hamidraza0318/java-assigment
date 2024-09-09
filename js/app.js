89f56c5bc8223b7b6bbf24f535e7e85e8bbcfd32
hamidraza0318 <159810777+hamidraza0318@users.noreply.github.com>
2024-09-09 17:54:43 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var  hello = "Error! Please enter a valid passwords";
+alert("Error! Please enter a valid passwords")
+var num = 23232323;
+alert (9929292292);
+
+// chapter string  2
+var username = ("enter your name ") ;
+  username = "jhon doy" ;
+alert (username) ;
+
+// java  men hum number me inverted quma men string men ikhenge """ ye na bhi lagay to code chlta he jubke alpha kelea ""' ZARORI HE 
+var abc = 2222; 
+abc = "10";
+alert(abc);
+
+// chapter 3 number
+
+var num1 = 3;
+var num2 = 5;
+var result = num1 * num2 ;
+alert (result)
