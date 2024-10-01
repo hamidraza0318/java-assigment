65d8ca23f2d8be1cb0ce63d881962dbb99ee8acc
hamidraza0318 <159810777+hamidraza0318@users.noreply.github.com>
2024-10-01 19:27:08 +0500

    Add files via upload

---
+++ assigment smit/app.js
@@ -0,0 +1,25 @@
+//   assigment 2
+// Declare 3 variables in one statement 
+// var a = 2, b = 3, c = 4
+// capter 4 legal and illigle 
+// Declare 5 legal and 5 illegal variables
+// q 1 2 3 
+
+// legal statment cavil case
+// var ah mad =( "hello") space bhi nahi 
+// alert{hamid}
+
+        // let name = "hamid";
+        // let f_username = "raza";snack case
+        // let interYourAge = 19;cavil case
+        // let _value = 100;
+        // let my_name = "raza"
+        
+        // ILLEGAL VARIABLES
+        // let my-name = "HASSAN";
+        // let @username = "admin";
+        // let 2ndPlace = "second";
+        // let var = 10; 
+        // let break = "break";
+        // Display this in your browser
+        // document.write("Rules for naming JS variables <br> <br> <br> Varaible name can only contain numbers, $ and_ . For example $my, _name or name <br> variables must begin with a letter, $ or_ . For example :$name , _name or name <br> Variables name are case sensitive <br> Variables names should not be JS  keywords")
